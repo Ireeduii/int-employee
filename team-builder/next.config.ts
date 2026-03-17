bf876693a8a25bb62bc47ac4c1b1a6c39287d47a
Ireeduii <25LP4866@nest.edu.mn>
2026-03-18 04:02:09 +0800

    fix: force skip lint and ts errors during build

--- team-builder/next.config.ts
+++ team-builder/next.config.ts
@@ -1,32 +1,44 @@
-// // import type { NextConfig } from "next";
+// // // import type { NextConfig } from "next";
+
+// // // const nextConfig: NextConfig = {
+// // //   /* config options here */
+// // // };
+
+// // // export default nextConfig;
+
+// // import { NextConfig } from "next";
+// // import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 // // const nextConfig: NextConfig = {
-// //   /* config options here */
+// //   //
 // // };
+
+// // if (process.env.NODE_ENV === "development") {
+// //   setupDevPlatform();
+// // }
 
 // // export default nextConfig;
 
-// import { NextConfig } from "next";
-// import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
-
-// const nextConfig: NextConfig = {
-//   //
+// const nextConfig = {
+//   eslint: {
+//     // Build хийх үед ESLint-ийг алдаа заасан ч үргэлжлүүл гэж хэлж байна
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     // TypeScript-ийн алдаа байсан ч Build-ийг зогсоохгүй
+//     ignoreBuildErrors: true,
+//   },
 // };
-
-// if (process.env.NODE_ENV === "development") {
-//   setupDevPlatform();
-// }
 
 // export default nextConfig;
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Build хийх үед ESLint-ийг алдаа заасан ч үргэлжлүүл гэж хэлж байна
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    // TypeScript-ийн алдаа байсан ч Build-ийг зогсоохгүй
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true, // TypeScript алдааг үл тоомсорлох
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // ESLint алдааг үл тоомсорлох
   },
 };
 
