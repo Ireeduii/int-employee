60c5b0500890b6a2433ced7f13819c17113cc7b3
Ireeduii <25LP4866@nest.edu.mn>
2026-03-18 04:05:57 +0800

    fix: disable eslint rules in flat config

--- team-builder/eslint.config.mjs
+++ team-builder/eslint.config.mjs
@@ -1,3 +1,29 @@
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     ignores: [
+//       "node_modules/**",
+//       ".next/**",
+//       "out/**",
+//       "build/**",
+//       "next-env.d.ts",
+//     ],
+//   },
+// ];
+
+// export default eslintConfig;
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +37,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // Энд дүрмүүдийг идэвхгүй болгож байна
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "prefer-const": "off",
+    },
+  },
   {
     ignores: [
       "node_modules/**",
